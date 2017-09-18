029d616679a44c36c1693281b21ea88738efe1d2
Oscar Hernández Omaña <oscar.h.o@outlook.com>
2017-09-18 18:30:37 -0500

    Commit 9.1
    Nuevo tema, codigo listo

---
+++ Ejercicios/9. Preguntas y respuestas básicas/PreguntasRespuestasBasicas/PreguntasRespuestasBasicas/Dialogs/RootDialog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace PreguntasRespuestasBasicas.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            //string message = activity.Text.ToLower();
+            //string id = activity.From.Id;
+
+            // calculate something for us to return
+            //int length = (activity.Text ?? string.Empty).Length;
+
+            //// return our reply to the user
+            //await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
