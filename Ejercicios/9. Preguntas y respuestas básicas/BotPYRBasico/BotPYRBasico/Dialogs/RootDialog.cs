ae14ad883541d5c47b2002247d1322e84b8df73f
Oscar Hernández Omaña <oscar.h.o@outlook.com>
2017-09-26 19:09:07 -0500

    Commit 9.4
    code autualizado

---
+++ Ejercicios/9. Preguntas y respuestas básicas/BotPYRBasico/BotPYRBasico/Dialogs/RootDialog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace BotPYRBasico.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+
+            string message = activity.Text.ToLower();
+
+
+            switch (message)
+            {
+                case "hola":
+                    await context.PostAsync("Hola señor!");
+                    break;
+                case "como te llamas?":
+                    await context.PostAsync("mi nombre es bot");
+                    break;
+                case "cuál es tu deporte favorito?":
+                    await context.PostAsync("¿Deporte? ¿Qué es eso? ¿Escribir código cuenta como deporte?");
+                    break;
+                case "donde estoy?":
+                    await context.PostAsync("En GitHub");
+                    break;
+                default:
+                    await context.PostAsync("Lo siento, no entendí esto");
+                    break;
+            }
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
---
+++ Ejercicios/9. Preguntas y respuestas básicas/BotPYRBasico/BotPYRBasico/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BotPYRBasico.WebApiApplication" Language="C#" %>
